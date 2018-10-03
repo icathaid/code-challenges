0c1ba2e01a57ade480a17c01f196d7b8e0e16948
James McDaniel <icathaid@gmail.com>
2018-10-02 21:00:40 -0700

    api

--- promises/challenge.spec.js
+++ promises/challenge.spec.js
@@ -284,12 +284,11 @@ describe('chaining multiple promises can enhance readability', () => {
     });
   
     it('a function given to `then()` fulfills (if it doesnt throw)', function() {
-      const beNice = Promise.resolve('I am nice');
-      return Promise.resolve('I am nice')
+      const beNice = () => { return Promise.resolve('I am nice') };
+      return Promise.resolve(beNice())
         .then(beNice)
         .then(niceMessage => assert.equal(niceMessage, 'I am nice'))
     });
-    
   });
 
   describe('chain promises', function() {
@@ -354,6 +353,73 @@ describe('chaining multiple promises can enhance readability', () => {
       ;
     });
     
+  });
+
+});
+
+
+// 78: Promise - API overview
+// To do: make all tests pass, leave the assert lines unchanged!
+
+describe('`Promise` API overview', function() {
+
+  it('`new Promise()` requires a function as param', () => {
+    const param = () => {};
+    assert.doesNotThrow(() => { new Promise(param); });
+  });
+
+  describe('resolving a promise', () => {
+    // reminder: the test passes when a fulfilled promise is returned
+    it('via constructor parameter `new Promise((resolve) => { resolve(); })`', () => {
+      const param = (resolve) => { resolve(); };
+      return new Promise(param);
+    });
+    it('using `Promise.resolve()`', () => {
+      return Promise.resolve(new Error('stuff'));
+    });
+  });
+
+  describe('a rejected promise', () => {
+    it('using the constructor parameter', (done) => {
+      const promise = new Promise(() => { reject(); });
+      promise
+        .then(() => done(new Error('The promise is expected to be rejected.')))
+        .catch(() => done());
+    });
+    it('via `Promise.reject()`', (done) => {
+      const promise = Promise.reject();
+      promise
+        .then(() => done(new Error('The promise is expected to be rejected.')))
+        .catch(() => done());
+    });
+  });
+
+  const resolvingPromise = Promise.resolve();
+  const rejectingPromise = Promise.reject();
+
+  describe('`Promise.all()`', () => {
+    it('`Promise.all([p1, p2])` resolves when all promises resolve', () =>
+      Promise.all([resolvingPromise, resolvingPromise, resolvingPromise])
+    );
+    it('`Promise.all([p1, p2])` rejects when a promise is rejected', (done) => {
+      Promise.all([resolvingPromise, rejectingPromise])
+        .then(() => done(new Error('The promise is expected to be rejected.')))
+        .catch(() => done())
+    });
+  });
+
+  describe('`Promise.race()`', () => {
+    it('`Promise.race([p1, p2])` resolves when one of the promises resolves', () =>
+      Promise.race([resolvingPromise, rejectingPromise, resolvingPromise])
+    );
+    it('`Promise.race([p1, p2])` rejects when one of the promises rejects', (done) => {
+      Promise.race([rejectingPromise, resolvingPromise])
+        .then(() => done(new Error('The promise is expected to be rejected.')))
+        .catch(() => done())
+    });
+    it('`Promise.race([p1, p2])` order matters (and timing)', () =>
+      Promise.race([setTimeout(rejectingPromise, 100), resolvingPromise])
+    );
   });
 
 });
