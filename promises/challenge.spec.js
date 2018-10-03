afc57a75ff13795ed3982ffe8587a60b8e33bcd6
James McDaniel <icathaid@gmail.com>
2018-10-02 23:14:29 -0700

    computed values -stuck on setter function

--- promises/challenge.spec.js
+++ promises/challenge.spec.js
@@ -43,3 +43,42 @@ describe('The object literal allows for new shorthands', () => {
   });
   
 });
+
+
+
+// 16: object-literal - computed properties
+// To do: make all tests pass, leave the assert lines unchanged!
+
+describe('Object literal properties may be computed values', () => {
+
+  it('a computed property `x` needs to be surrounded by `[]`', () => {
+    const propertyName = 'x';
+    const obj = {[propertyName]: 1};
+    assert.equal(obj.x, 1);
+  });
+
+  it('can also get a function assigned', () => {
+    const key = 'func';
+    const obj = {[key](){return 'seven'}};
+    assert.equal(obj.func(), 'seven');
+  });
+
+  it('the key may also be the result of a function call', () => {
+    const getName = () => {return 'propertyName'};
+    const obj = {[getName()]() {return 'seven'}};
+    assert.equal(obj.propertyName(), 'seven');
+  });
+
+  it('the key can also be constructed by an expression', () => {
+    const wat = 'tyName';
+    const obj = {['proper' + wat]: true};
+    assert.equal('propertyName' in obj, true);
+  });
+
+  it('accessor keys can be computed names too', () => {
+    const obj = {
+      set ['key'](_) {return 1}
+    };
+    assert.equal(obj.key, 1);
+  });
+});
