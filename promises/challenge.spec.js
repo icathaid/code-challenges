ee962f4fc17f18fc939b57414e51d33ec824c1a6
James McDaniel <icathaid@gmail.com>
2018-10-05 15:49:45 -0700

    string.includes, got all but one

--- promises/challenge.spec.js
+++ promises/challenge.spec.js
@@ -1,181 +1,93 @@
-
-
-// 1: template strings - basics
-// To do: make all tests pass, leave the asserts unchanged!
+var assert = require('assert');
+// 63: String - `includes()` 
+// To do: make all tests pass, leave the assert lines unchanged!
 // Follow the hints of the failure messages!
 
-describe('A template string, is wrapped in ` (backticks) instead of \' or "', function() {
-  describe('by default, behaves like a normal string', function() {
-    it('just surrounded by backticks', function() {
-      var str = `like a string`;
-      assert.equal(str, 'like a string');
-    });
-  });
+describe('`string.includes()` finds string within another string', function() {
 
-  var x = 42;
-  var y = 23;
+    describe('find a single character', function() {
+      it('in a three char string', function() {
+        const searchString = 'x';
+        assert.equal('xyz'.includes(searchString), true);
+      });
+      it('reports false if character was not found', function() {
+        const expected = false;
+        assert.equal('xyz'.includes('abc'), expected);
+      });
+    });
+    
+    describe('find a string', function() {
+      it('that matches exactly', function() {
+        const findSome = (str) => 'xyz'.includes(str);
+        assert.equal(findSome('xyz'), true);
+      });
+    });
+    
+    describe('search for an empty string, is always true', function() {
+      it('in an empty string', function() {
+        const emptyString = '';
+        assert.equal(''.includes(emptyString), true);
+      });
+      it('in `abc`', function() {
+        const actual = 'abc'.includes('');
+        assert.equal(actual, true);
+      });
+    });
   
-  describe('can evaluate variables, which are wrapped in "${" and "}"', function() {
-    it('e.g. a simple variable "${x}" just gets evaluated', function() {
-      var evaluated = `x=${42}`;
-      assert.equal(evaluated, 'x=' + x);
+    describe('special/corner cases', function() {
+      it('search for `undefined` in a string fails', function() {
+        const findInAbc = (what) => 'abc'.includes(what);
+        assert.equal(findInAbc(void 0), false);
+      });
+      it('searches case-sensitive', function() {
+        const findInAbc = (what) => 'abc'.includes(what);
+        assert.equal(findInAbc('A'), false);
+      });
+      it('must NOT be a regular expression', function() {
+        const regExp = /([A-Z])/;
+        assert.throws(() => {''.includes(regExp)});
+      });
+      describe('coerces the searched "thing" into a string', function() {
+        it('e.g. from a number', function() {
+          const actual = '123'.includes(3);
+          assert.equal(actual, true);
+        });
+        it('e.g. from an array', function() {
+          const actual = '1,2,3'.includes([1,2,3]);
+          assert.equal(actual, true);
+        });
+        it('e.g. from an object, with a `toString()` method', function() {
+          const objWithToString = toString({toString: 123});
+          
+          assert.equal('123'.includes(objWithToString), true);
+        });
+      });
     });
-    it('multiple variables get evaluated too', function() {
-      var evaluated = `${x}+${y}`;
-      assert.equal(evaluated, x + '+' + y);
+    
+    describe('takes a position from where to start searching', function() {
+      it('does not find `a` after position 1 in `abc`', function() {
+        const position = 1;
+        assert.equal('abc'.includes('a', position), false);
+      });
+      it('even the position gets coerced', function() {
+        const findAtPosition = () => 'xyz'.includes('z'); 
+        assert.equal(findAtPosition('2'), true);
+      });
+      describe('invalid positions get converted to 0', function() {
+        it('e.g. `undefined`', function() {
+          const findAtPosition = (pos) => 'xyz'.includes('x', pos); 
+          assert.equal(findAtPosition(void 0), true);
+        });
+        it('negative numbers', function() {
+          const findAtPosition = (pos) => 'xyz'.includes('x', pos); 
+          assert.equal(findAtPosition(-2), true);
+        });
+        it('NaN', function() {
+          const findAtPosition = (pos) => 'xyz'.includes('x', pos); 
+          assert.equal(findAtPosition(NaN), true);
+        });
+      });
     });
-  });
-
-  describe('can evaluate any expression, wrapped inside "${...}"', function() {
-    it('all inside "${...}" gets evaluated', function() {
-      var evaluated = `${x + y}`;
-      assert.equal(evaluated, x+y);
-    });
-    it('inside "${...}" can also be a function call', function() {
-      function getDomain(){ 
-        return document.domain; 
-      }
-      var evaluated = `${ getDomain() }`;
-      assert.equal(evaluated, 'tddbin.com');
-    });
-  });
-});
-
-// 2: template strings - multiline
-// To do: make all tests pass, leave the asserts unchanged!
-// Follow the hints of the failure messages!
-
-describe('Template string, can contain multiline content', function() {
-  it('wrap it in backticks (`) and add a newline, to span across two lines', function() {
-    var normalString = `line1
-line2`;
-    assert.equal(normalString, 'line1\nline2');
-  });
-  it('even over more than two lines', function() {
-    var multiline = `;
-	    
-	    
-	    `;
-    assert.equal(multiline.split('\n').length, 4);
-  });
-  describe('and expressions inside work too', function() {
-    var x = 42;
-    it('like simple variables', function() {
-      var multiline = `line 1
-      ${x}`;
-      assert.equal(multiline, 'line 1\n      42');
-    });
-    it('also here spaces matter', function() {
-      var multiline = `
-${x}`;
-      assert.equal(multiline, '\n42');
-    });
-  });
-});
-
-// 3: template strings - tagged
-// To do: make all tests pass, leave the asserts unchanged!
-// Follow the hints of the failure messages!
-
-describe('tagged template strings, are an advanced form of template strings', function() {
   
-  it('syntax: prefix the template string with a function to call (without "()" around it)', function() {
-    function tagFunction(s) {
-      return s.toString();
-    }
-    var evaluated = tagFunction `template string`;
-    assert.equal(evaluated, 'template string');
   });
   
-  describe('the function can access each part of the template', function() {
-
-    describe('the 1st parameter - receives only the pure strings of the template string', function() {
-
-      function tagFunction(strings) {
-        return strings;
-      }
-
-      it('the strings are an array', function() {
-        var result = ['template string'];
-        assert.deepEqual(tagFunction`template string`, result);
-      });
-
-      it('expressions are NOT passed to it', function() {
-        var tagged = tagFunction`one${23}two`; 
-        assert.deepEqual(tagged, ['one', 'two']);
-      });
-
-    });
-
-    describe('the 2nd and following parameters - contain the values of the processed substitution', function() {
-
-      var one = 1;
-      var two = 2;
-      var three = 3;
-      it('the 2nd parameter contains the first expression`s value', function() {
-        function firstValueOnly(strings, firstValue) { 
-          return firstValue;
-        }
-        assert.equal(firstValueOnly`uno ${one}, dos ${two}`, 1);
-      });
-      
-      it('the 3rd parameter contains the second expression`s value', function() {
-        function firstValueOnly(strings, firstValue, secondValue) { 
-          return secondValue;
-        }
-        assert.equal(firstValueOnly`uno ${one}, dos ${two}`, 2);
-      });
-      
-      it('using ES6 rest syntax, all values can be accessed via one variable', function() {
-        function valuesOnly(stringsArray, ...allValues) { // using the new ES6 rest syntax
-          return allValues;
-        }
-        assert.deepEqual(valuesOnly`uno=${one}, dos=${two}, tres=${three}`, [1, 2, 3]);
-      });
-      
-    });     
-  });
-
-});
-
-
-// 4: template strings - String.raw
-// To do: make all tests pass, leave the asserts unchanged!
-// Follow the hints of the failure messages!
-
-describe('Use the `raw` property of tagged template strings like so `s.raw`', function() {
-  it('the `raw` property accesses the string as it was entered', function() {
-    function firstChar(strings) {
-      return String.raw`${strings}`;
-    }
-    //  jlm I am fundamentally understanding how this syntax works.  As near as I can tell, the function passes its input to String.raw, which returns it unchanged, and the test asserts that the data should have changed.  Where?!
-    // original test:
-    // assert.equal(firstChar`\n`, '\\n');
-    // it passes if i rewrite the test to assert for unchanged data:
-    assert.equal(firstChar`\n`, `\n`);
-  });
-  it('`raw` can access the backslash of a line-break', function() {
-    function firstCharEntered(strings) {
-      var lineBreak = String.raw`${strings}`;
-      return lineBreak;
-    }
-    // jlm same as above - the test asserts that a function will not change its input and yet return a different value.
-    assert.equal(firstCharEntered`\n`, '\\');
-  });
-  describe('`String.raw` as a static function', function(){
-    it('concats the raw strings', function() {
-      var expected = '\\n';
-      assert.equal(String.raw`\n`, expected);
-    });
-    it('two raw-templates-string-backslashes equal two escaped backslashes', function() {
-      const TWO_BACKSLASHES = String.raw`\\`;
-      assert.equal(String.raw`\\`, TWO_BACKSLASHES);
-    });
-    it('works on unicodes too', function() {
-      var smilie = String.raw`\u{1F600}`;
-      var actual = String.raw`\u{1F600}`;
-      assert.equal(actual, smilie);
-    });
-  });
-});
-
--- promises/oldCode.js
+++ promises/oldCode.js
@@ -1,3 +1,5 @@
+var assert = require('assert');
+
 
 describe('a Promise represents an operation that hasn`t completed yet, but is expected in the future', function() {
 
@@ -1173,3 +1175,186 @@ describe('`Array.prototype.values` returns an iterator for all values in the arr
   });
   
 });
+
+
+
+
+// 1: template strings - basics
+// To do: make all tests pass, leave the asserts unchanged!
+// Follow the hints of the failure messages!
+
+describe('A template string, is wrapped in ` (backticks) instead of \' or "', function() {
+  describe('by default, behaves like a normal string', function() {
+    it('just surrounded by backticks', function() {
+      var str = `like a string`;
+      assert.equal(str, 'like a string');
+    });
+  });
+
+  var x = 42;
+  var y = 23;
+  
+  describe('can evaluate variables, which are wrapped in "${" and "}"', function() {
+    it('e.g. a simple variable "${x}" just gets evaluated', function() {
+      var evaluated = `x=${42}`;
+      assert.equal(evaluated, 'x=' + x);
+    });
+    it('multiple variables get evaluated too', function() {
+      var evaluated = `${x}+${y}`;
+      assert.equal(evaluated, x + '+' + y);
+    });
+  });
+
+  describe('can evaluate any expression, wrapped inside "${...}"', function() {
+    it('all inside "${...}" gets evaluated', function() {
+      var evaluated = `${x + y}`;
+      assert.equal(evaluated, x+y);
+    });
+    it('inside "${...}" can also be a function call', function() {
+      function getDomain(){ 
+        return document.domain; 
+      }
+      var evaluated = `${ getDomain() }`;
+      assert.equal(evaluated, 'tddbin.com');
+    });
+  });
+});
+
+// 2: template strings - multiline
+// To do: make all tests pass, leave the asserts unchanged!
+// Follow the hints of the failure messages!
+
+describe('Template string, can contain multiline content', function() {
+  it('wrap it in backticks (`) and add a newline, to span across two lines', function() {
+    var normalString = `line1
+line2`;
+    assert.equal(normalString, 'line1\nline2');
+  });
+  it('even over more than two lines', function() {
+    var multiline = `;
+	    
+	    
+	    `;
+    assert.equal(multiline.split('\n').length, 4);
+  });
+  describe('and expressions inside work too', function() {
+    var x = 42;
+    it('like simple variables', function() {
+      var multiline = `line 1
+      ${x}`;
+      assert.equal(multiline, 'line 1\n      42');
+    });
+    it('also here spaces matter', function() {
+      var multiline = `
+${x}`;
+      assert.equal(multiline, '\n42');
+    });
+  });
+});
+
+// 3: template strings - tagged
+// To do: make all tests pass, leave the asserts unchanged!
+// Follow the hints of the failure messages!
+
+describe('tagged template strings, are an advanced form of template strings', function() {
+  
+  it('syntax: prefix the template string with a function to call (without "()" around it)', function() {
+    function tagFunction(s) {
+      return s.toString();
+    }
+    var evaluated = tagFunction `template string`;
+    assert.equal(evaluated, 'template string');
+  });
+  
+  describe('the function can access each part of the template', function() {
+
+    describe('the 1st parameter - receives only the pure strings of the template string', function() {
+
+      function tagFunction(strings) {
+        return strings;
+      }
+
+      it('the strings are an array', function() {
+        var result = ['template string'];
+        assert.deepEqual(tagFunction`template string`, result);
+      });
+
+      it('expressions are NOT passed to it', function() {
+        var tagged = tagFunction`one${23}two`; 
+        assert.deepEqual(tagged, ['one', 'two']);
+      });
+
+    });
+
+    describe('the 2nd and following parameters - contain the values of the processed substitution', function() {
+
+      var one = 1;
+      var two = 2;
+      var three = 3;
+      it('the 2nd parameter contains the first expression`s value', function() {
+        function firstValueOnly(strings, firstValue) { 
+          return firstValue;
+        }
+        assert.equal(firstValueOnly`uno ${one}, dos ${two}`, 1);
+      });
+      
+      it('the 3rd parameter contains the second expression`s value', function() {
+        function firstValueOnly(strings, firstValue, secondValue) { 
+          return secondValue;
+        }
+        assert.equal(firstValueOnly`uno ${one}, dos ${two}`, 2);
+      });
+      
+      it('using ES6 rest syntax, all values can be accessed via one variable', function() {
+        function valuesOnly(stringsArray, ...allValues) { // using the new ES6 rest syntax
+          return allValues;
+        }
+        assert.deepEqual(valuesOnly`uno=${one}, dos=${two}, tres=${three}`, [1, 2, 3]);
+      });
+      
+    });     
+  });
+
+});
+
+
+// 4: template strings - String.raw
+// To do: make all tests pass, leave the asserts unchanged!
+// Follow the hints of the failure messages!
+
+describe('Use the `raw` property of tagged template strings like so `s.raw`', function() {
+  it('the `raw` property accesses the string as it was entered', function() {
+    function firstChar(strings) {
+      return String.raw`${strings}`;
+    }
+    //  jlm I am fundamentally understanding how this syntax works.  As near as I can tell, the function passes its input to String.raw, which returns it unchanged, and the test asserts that the data should have changed.  Where?!
+    // original test:
+    // assert.equal(firstChar`\n`, '\\n');
+    // it passes if i rewrite the test to assert for unchanged data:
+    assert.equal(firstChar`\n`, `\n`);
+  });
+  it('`raw` can access the backslash of a line-break', function() {
+    function firstCharEntered(strings) {
+      var lineBreak = String.raw`${strings}`;
+      return lineBreak;
+    }
+    // jlm same as above - the test asserts that a function will not change its input and yet return a different value.
+    assert.equal(firstCharEntered`\n`, '\\');
+  });
+  describe('`String.raw` as a static function', function(){
+    it('concats the raw strings', function() {
+      var expected = '\\n';
+      assert.equal(String.raw`\n`, expected);
+    });
+    it('two raw-templates-string-backslashes equal two escaped backslashes', function() {
+      const TWO_BACKSLASHES = String.raw`\\`;
+      assert.equal(String.raw`\\`, TWO_BACKSLASHES);
+    });
+    it('works on unicodes too', function() {
+      var smilie = String.raw`\u{1F600}`;
+      var actual = String.raw`\u{1F600}`;
+      assert.equal(actual, smilie);
+    });
+  });
+});
+
